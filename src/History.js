f536efd3b84b2546ad855b905c5ea7f6f0911761
Michel descotes <micheldescotes65@gmail.com>
2018-10-15 20:41:56 +0200

    creation du router

---
+++ src/History.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class History extends Component {
+    render (){
+        return (
+            <h1>Notre histoire</h1>
+        )
+    }
+}
+
+export default History;
